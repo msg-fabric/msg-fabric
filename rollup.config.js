8557957957f9ce90d434acebe760940ed9d9b4f8
Shane Holloway <shane.holloway@ieee.org>
2018-02-17 20:08:58 -0700

    Updated to use msg-fabric-core lean branch

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,32 @@
-import rpi_babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+import {minify} from 'uglify-es'
+import rpi_uglify from 'rollup-plugin-uglify'
 import rpi_resolve from 'rollup-plugin-node-resolve'
 
 const sourcemap = 'inline'
+const plugins = [rpi_resolve({module: true, jsnext: true})]
 
-const external = ['crypto', 'url', 'stream', 'net', 'tls']
-
-const plugins = [jsy_plugin(), rpi_resolve({module: true, jsnext: true})]
+const ugly = { warnings: true, output: {comments: false, max_line_len: 256}}
+const prod_plugins = plugins.concat([rpi_uglify(ugly, minify)])
 
 export default [
 	{ input: 'code/msg-fabric-node.jsy',
 		output: [
-      { file: `dist/msg-fabric-node.js`, format: 'cjs' },
-      { file: `dist/msg-fabric-node.mjs`, format: 'es' },
+      { file: pkg.main, format: 'cjs', sourcemap },
+      { file: pkg.module, format: 'es', sourcemap },
     ],
-    sourcemap, external, plugins },
+    external: ['crypto', 'url', 'stream', 'net', 'tls'], plugins },
 
 	{ input: 'code/msg-fabric-browser.jsy',
-    name: 'msg-fabric-browser',
 		output: [
-      { file: `dist/msg-fabric-browser.js`, format: 'cjs' },
-      { file: `dist/msg-fabric-browser.mjs`, format: 'es' },
-      { file: `dist/msg-fabric-browser.umd.js`, format: 'umd' },
+      { file: `cjs/msg-fabric-browser.js`, format: 'cjs', sourcemap },
+      { file: `esm/msg-fabric-browser.js`, format: 'es', sourcemap },
     ],
-    sourcemap, external, plugins },
-]
+    external: [], plugins },
 
-
-
-
-function jsy_plugin() {
-  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
-  return rpi_babel({
-    exclude: 'node_modules/**',
-    presets: [ jsy_preset ],
-    plugins: [],
-    babelrc: false }) }
+    prod_plugins &&
+      { input: 'code/msg-fabric-browser.jsy',
+        output: { file: pkg.browser, name:'msg-fabric', format: 'umd', sourcemap },
+        external: [], plugins: prod_plugins },
+].filter(e => e)
 
